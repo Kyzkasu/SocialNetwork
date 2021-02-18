0155e10c04b0e1ad75e88736230388bf7d6491ea
mle <minhguyle@mail.com>
2021-02-18 11:47:51 +0100

    Commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Cours'`, () => {
+  it(`should have as title 'Socialy'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Cours');
+    expect(app.title).toEqual('Socialy');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Cours app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Socialy app is running!');
   });
 });
